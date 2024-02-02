171cd0ceadea7135d7b20b8b527ce514bca06d25
AshwinKumar Narendra Ubare <ashwinubare@gmail.com>
2024-02-02 17:45:04 +0530

    new project school_management_app

---
+++ JavaProjects/src/school_management_app/Student.java
@@ -0,0 +1,38 @@
+package school_management_app;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private int grade;
+	private int paidFees;
+	private int totalFees;
+	
+	/**
+	 * 
+	 * @param id
+	 * @param name
+	 * @param grade
+	 */
+	
+	public Student(int id, String name, int grade)
+	{
+		this.id=id;
+		this.name=name;
+		this.grade=grade;
+		this.totalFees=25000;
+		this.paidFees=0;
+	}
+	
+	public void setGreade(int grade)
+	{
+		this.grade=grade;
+	}
+	
+	public void updateFees(int fees)
+	{
+		paidFees=fees;
+	}
+	
+	
+}
