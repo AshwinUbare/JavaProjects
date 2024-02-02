7d1e8bf22d4f6275e62ad9375fb764665f988c64
AshwinKumar Narendra Ubare <ashwinubare@gmail.com>
2024-02-02 13:51:12 +0530

    Banking_App is done

--- JavaProjects/src/banking_app/BankAccount.java
+++ JavaProjects/src/banking_app/BankAccount.java
@@ -1,6 +1,7 @@
 package banking_app;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class BankAccount {
 
@@ -9,19 +10,23 @@ public class BankAccount {
 	private float balance;
 	private float previousTransaction;
 	
+	// for printing 2-digit decimal we use DecimalFormat class
+	private static final DecimalFormat df=new DecimalFormat("0.00");
+	
 	public BankAccount(String customerName, String customerId)
 	{
 		this.customerName=customerName;
 		this.customerId=customerId;
-		this.balance=0;
+		this.balance=1000;
 		this.previousTransaction=0;
 		
+		System.out.println("Welcome "+this.customerName);
 		System.out.println("Account Id: "+this.customerId);
 		System.out.println("Account Name: "+this.customerName);
 		
 	}
 	
-	//function which deposit the amount in account holder
+	//function which deposit the amount in account 
 	private void deposit(float amount)
 	{
 		 if(amount > 0)
@@ -29,7 +34,10 @@ public class BankAccount {
 			 balance=balance+amount;
 			 previousTransaction= amount;
 			 System.out.println("Amount is Deposit Successfully");
-			 System.out.println("Current Balance: "+balance);
+			 
+			 //we are sending balance to format function of decimal class and receiving balance with 2-decimal format
+			 // but this format class receive double data variable but we sending float data type
+			 System.out.println("Current Balance: "+df.format(balance)); // df is object
 		 }
 		 else
 		 {
@@ -37,6 +45,7 @@ public class BankAccount {
 		 }
 	}
 	
+	//function which withdraw the amount in account 
 	private void withdraw(float amount)
 	{
 		//This condition check first there is sufficient balance in account
@@ -45,7 +54,7 @@ public class BankAccount {
 			if(balance != 0 && balance >= amount) 
 			{
 				balance= balance - amount;
-				previousTransaction= -amount;
+				previousTransaction= -amount;  // here we store negative value to understand amount is withdraw
 				System.out.println("Amount is Withdraw Successfully");
 			}
 			else
@@ -57,31 +66,50 @@ public class BankAccount {
 		}
 	}
 	
+	private void getPreviousTransaction()
+	{
+		if(previousTransaction != 0)
+		{
+			if(previousTransaction >0)
+			{
+				System.out.println("Amount Deposited: "+previousTransaction);
+			}
+			else if(previousTransaction<0)
+			{
+				System.out.println("Amount Withdraw: "+Math.abs(previousTransaction));
+			}
+		}
+		else
+		{
+			System.out.println("No Transaction is Occured");
+		}
+			
+	}
+	
 	public String getBalance()
 	{
-		return "Account Balance: "+this.balance;
-	
+		return "Account Balance: "+df.format(this.balance);
 	}
 	
 	
 	// Showing option to user(Menu)
-	 void showMenu() // default is public
+	 void showMenu()                            // default is public
 	{
 		Scanner sc=new Scanner(System.in);
 		
 		int option;
 		do {
-		System.out.println("**************************************************************************");
+		System.out.println("\n**************************************************************************");
 		System.out.println("Select the option: ");
 		System.out.println("1 For Check Balance \n2 For Deposit the Money \n3 For Withdraw the Money \n4 For Previous Transaction \n0 Exit");
 		System.out.println("**************************************************************************");
-		System.out.print("Enter the choice: ");
+		System.out.print("\nEnter the choice: ");
 		option=sc.nextInt();
 		
 		switch(option)
 		{
 			case 1:
-				System.out.println("\nAccount Balance: "+balance);
+				System.out.println("\nAccount Balance: "+df.format(balance));
 				break;
 				
 			case 2:
@@ -96,14 +124,20 @@ public class BankAccount {
 				withdraw(wdAmount);
 				break;
 				
+			case 4:
+				getPreviousTransaction();
+				break;
+
+				
 			case 0:
 				System.out.println("\n**********Exit Successfully***********");
 				break;
 				
 			default:
-				System.out.println("\nWrong Choices");
+				System.out.println("\nWrong Choice");
 		}
 		}while(option != 0);
+		
 	}
 	
 	
--- JavaProjects/src/banking_app/BankingAppMain.java
+++ JavaProjects/src/banking_app/BankingAppMain.java
@@ -10,6 +10,5 @@ public class BankingAppMain {
 		a1.showMenu();
 		
 		
-		
 	}
 }
